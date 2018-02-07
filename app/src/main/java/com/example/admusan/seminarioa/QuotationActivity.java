7df60e00ec8e2981c2838284c6582263cc8810f9
admarcar <admarcar@inf.upv.es>
2018-02-07 19:56:45 +0100

    Seminario 3B Acts 1-6

--- app/src/main/java/com/example/admusan/seminarioa/QuotationActivity.java
+++ app/src/main/java/com/example/admusan/seminarioa/QuotationActivity.java
@@ -2,6 +2,8 @@ package com.example.admusan.seminarioa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,6 +15,25 @@ public class QuotationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quotation);
         TextView tv = findViewById(R.id.Quotation_quote);
         tv.setText(getString(R.string.Quotation_init_text,getResources().getString(R.string.nombre_usuario)));
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.action_bar_quotation,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.Add_favourites:
+                return true;
+            case R.id.New_quote:
+                TextView tv1 = findViewById(R.id.Quotation_quote);
+                TextView tv2 = findViewById(R.id.Quotation_author);
+                tv1.setText(R.string.Quotation_sample_quotation);
+                tv2.setText(R.string.Quotation_sample_author);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     void refresh_button_callback(View v){
---
+++ app/src/main/java/extras/Adaptador.java
@@ -0,0 +1,46 @@
+package extras;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.admusan.seminarioa.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by admarcar on 07/02/2018.
+ */
+
+public class Adaptador extends ArrayAdapter<Quotation>{
+
+    Context context;
+    int resource;
+    ArrayList<Quotation> data;
+
+    public Adaptador(Context context, int resource, ArrayList<Quotation> data){
+        super(context, resource, data);
+        this.context = context;
+        this.resource = resource;
+        this.data = data;
+    }
+
+    public View getView(final int posicion, View convertView, ViewGroup parent){
+        View view = convertView;
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.quotation_list_row, null);
+        }
+        Quotation d = data.get(posicion);
+        TextView tv1 = (TextView) view.findViewById(R.id.texto_cita);
+        tv1.setText(d.getCita());
+        TextView tv2 = (TextView) view.findViewById(R.id.texto_autor);
+        tv2.setText(d.getAutor());
+        return view;
+    }
+}
