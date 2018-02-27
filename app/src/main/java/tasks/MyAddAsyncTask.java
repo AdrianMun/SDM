8118414edceec7fa3512ee223275a5b85ade1b5c
Adrian <adrian.mun.s@gmail.com>
2018-02-27 18:28:59 +0100

    Ejercicio 4 hecho

--- app/src/main/java/tasks/MyAddAsyncTask.java
+++ app/src/main/java/tasks/MyAddAsyncTask.java
@@ -10,6 +10,7 @@ import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -37,27 +38,63 @@ public class MyAddAsyncTask extends AsyncTask<Void, Void, Quotation> {
         builder.appendPath("api");
         builder.appendPath("1.0");
         builder.appendPath(""); //Anyade el extra /
-        builder.appendQueryParameter("method","getQuote");
-        builder.appendQueryParameter("format","json");
+
+
         if(ref.get() == null) return null;
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ref.get());
+
+        if(prefs.getString("http_insertado","0").equals("0")) {
+
+        builder.appendQueryParameter("method","getQuote");
+        builder.appendQueryParameter("format","json");
+
         if(prefs.getString("idioma_insertado","0").equals("0")) builder.appendQueryParameter("lang", "en");
         else builder.appendQueryParameter("lang", "ru");
-        try {
-            URL url = new URL(builder.build().toString());
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            System.err.println(connection.getResponseCode() + " - " + builder.build().toString());
-            if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                Gson gson = new Gson();
-                Quotation q = gson.fromJson(reader, Quotation.class);
-                reader.close();
-                return q;
+
+            try {
+                URL url = new URL(builder.build().toString());
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                System.err.println(connection.getResponseCode() + " - " + builder.build().toString());
+                if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    Gson gson = new Gson();
+                    Quotation q = gson.fromJson(reader, Quotation.class);
+                    reader.close();
+                    return q;
+                }
+                connection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+        else {
+
+            String params="method=getQuote&format=json";
+            if(prefs.getString("idioma_insertado","0").equals("0")) params=params.concat("&lang=en");
+            else params=params.concat("&lang=ru");
+
+            try {
+                URL url = new URL(builder.build().toString());
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+                writer.write(params);
+                writer.flush();
+                writer.close();
+                System.err.println(connection.getResponseCode() + " - " + builder.build().toString());
+                if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    Gson gson = new Gson();
+                    Quotation q = gson.fromJson(reader, Quotation.class);
+                    reader.close();
+                    return q;
+                }
+                connection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
